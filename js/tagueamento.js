d122e4593fdff5b14adf8dfdb4e70f3ce1d497f7
Rafael Marques <r.mateusmarques@gmail.com>
2022-01-31 23:39:23 -0300

    mudanca de tracking UA

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -5,7 +5,5 @@
         m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
 })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-ga('create', 'G-RN3JZDHKZK', 'auto');
+ga('create', 'UA-219135896-1', 'auto');
 ga('send', 'pageview');
-
-alert('O script foi executado!')

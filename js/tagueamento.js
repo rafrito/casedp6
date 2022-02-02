c8605d16b7f7d9ad5e41b064a8cd978715929ad5
Rafael Marques <r.mateusmarques@gmail.com>
2022-02-02 00:47:48 -0300

    eventos de preenchimento de contato

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,3 +1,5 @@
+// Define a função global ga
+
 (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
         (i[r].q = i[r].q || []).push(arguments)
@@ -5,5 +7,22 @@
         m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
 })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
+// Configura tracker e envia pageview
+
 ga('create', 'UA-219135896-1', 'auto');
 ga('send', 'pageview');
+
+
+// Envia eventos de clique no formulário
+
+let formContato = document.querySelector("form.contato")
+
+for (element of formContato.querySelectorAll("li")) {
+
+    const label = element.querySelector("label").textContent
+    element.querySelector("input").onchange = () => {
+        ga('send', 'event', "contato", label, "preencheu");     
+    };
+}
+
+// ga('send', 'event', [eventCategory], [eventAction], [eventLabel], [eventValue], [fieldsObject]);

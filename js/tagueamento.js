bef9f13a6b478e34e64dbd55021fab4bf927de52
Rafael Marques <r.mateusmarques@gmail.com>
2022-02-02 04:43:07 -0300

    todos eventos inseridos

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,51 +1,95 @@
-// Define a função global ga
+// Define a função global ga, tracker e envia pageview
+const setGAPageview = () => {
+    (function (i, s, o, g, r, a, m) {
+        i["GoogleAnalyticsObject"] = r; i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
 
-(function (i, s, o, g, r, a, m) {
-    i["GoogleAnalyticsObject"] = r; i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date(); a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-})(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
+    ga("create", "UA-219135896-1", "auto");
+    ga("send", "pageview");
 
-
-// Configura tracker e envia pageview
-
-ga("create", "UA-219135896-1", "auto");
-ga("send", "pageview");
+}
 
 
 // Associa labels a elementos
-
-let labels = document.getElementsByTagName("label");
-for (label of labels) {
-    if (label.htmlFor != '') {
-         let elem = document.getElementById(label.htmlFor);
-         if (elem)
-            elem.label = label;         
-    }
-}
+const setLabels = () => {
+    let labels = document.getElementsByTagName("label");
+    for (label of labels) {
+        if (label.htmlFor != '') {
+            let elem = document.getElementById(label.htmlFor);
+            if (elem)
+                elem.label = label;         
+        };
+    };
+};
 
 
 // Envia eventos do menu
+const setEventsMenu = () => {
 
-document.querySelector(".menu-lista-contato").onclick = () => {
-    ga("send", "event", "menu", "entre_em_contato", "link_externo");
-}
+    document.querySelector(".menu-lista-contato").onclick = () => {
+        ga("send", "event", "menu", "entre_em_contato", "link_externo");
+    };
 
-document.querySelector(".menu-lista-download").onclick = () => {
-    ga("send", "event", "menu", "download_pdf", "download_pdf");
-}
+    document.querySelector(".menu-lista-download").onclick = () => {
+        ga("send", "event", "menu", "download_pdf", "download_pdf");
+    };
+};
 
 
 // Envia eventos de clique no formulário
+const setEventsContato = () => {
 
-let formContato = document.querySelector("form.contato")
+    setLabels()
+    let formContato = document.querySelector("form.contato");
+    for (let input of formContato.querySelectorAll("input")) {
 
-for (input of formContato.querySelectorAll("input")) {
+        input.onchange = () => {
+            ga("send", "event", "contato", input.label.textContent, "preencheu");
+        };
+    };
 
-    input.onchange = () => {
-        ga("send", "event", "contato", input.label.textContent, "preencheu");
+    // Popup
+    const mutationCallback = (mutationsList) => {
+        for (const mutation of mutationsList) {
+            mutation.type == "attributes" &&
+            mutation.attributeName == "class" &&
+            mutation.target.classList.contains("lightbox-open") &&
+            ga("send", "event", "contato", "enviado", "enviado");
+        };
+    };
+    
+    const observer = new MutationObserver(mutationCallback)
+    observer.observe(document.body, { attributes: true })
+};
+
+
+// Envia eventos de clique em Análise
+const setEventsAnalise = () => {
+
+    for (let card of document.querySelectorAll(".card-montadoras")) {
+        console.log(card)
+        card.onclick = () => {
+            ga("send", "event", "analise", "ver_mais", card.getAttribute("data-name"));
+        };
     };
 }
 
-// ga('send', 'event', [eventCategory], [eventAction], [eventLabel], [eventValue], [fieldsObject]);
+const main = () => {
+
+    setGAPageview()
+    setEventsMenu()
+
+    const eventosPaginas = {
+        "/sobre": setEventsContato,
+        "/analise": setEventsAnalise
+    }
+
+    let pagina = window.location.pathname
+
+    pagina == '/' || eventosPaginas[pagina]()
+}
+
+main()
